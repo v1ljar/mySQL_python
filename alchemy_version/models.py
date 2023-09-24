73aef6c7f9723452f5e7606ecc7e6195fb854184
Viljar Uljas <viljar1990@gmail.com>
2023-09-24 10:46:17 +0300

    sqlalchemy models updated

---
+++ alchemy_version/models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
+
+eng = create_engine("sqlite:///todo_app.db")
+Base = declarative_base()
+
+
+class TodoLists(Base):
+    __tablename__ = "todo_lists"
+
+    list_id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(255), nullable=False)
+
+
+class TodoItems(Base):
+    __tablename__ = "todo_items"
+
+    item_id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(255), nullable=False)
+    list_id = Column(Integer, ForeignKey(
+        "todo_lists.list_id", ondelete="CASCADE"))
+    state = Column(Boolean, default=False)
+
+
+Base.metadata.create_all(eng)
